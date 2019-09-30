02e443b7392386fa1fdf7b2106bae347ea20c8c0
hex <yiwen9999@live.cn>
2019-09-30 14:44:58 +0800

    update Personnel class add f3 function

--- src/main/java/com/hex/demogit/dataobject/Personnel.java
+++ src/main/java/com/hex/demogit/dataobject/Personnel.java
@@ -6,4 +6,10 @@ package com.hex.demogit.dataobject;
  * Time: 3:37 下午
  */
 public class Personnel {
+
+    public void f3(){
+        System.out.println("this is function 3");
+        System.out.println("回退版本后对该版本进行新的修改后尝试上传。");
+    }
+
 }
